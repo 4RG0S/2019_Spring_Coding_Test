f928277edc072ab08b25c7bf2dd10a1420ca4771
leebyeongman <ssey0921@gmail.com>
2019-04-19 03:02:45 +0900

    modify code

--- 이병만/1240. 단순 2진 암호코드/Solution.py
+++ 이병만/1240. 단순 2진 암호코드/Solution.py
@@ -4,24 +4,26 @@
 def convert_code(data):
     if data == '0001101':
         return 0
-    if data == '0011001':
+    elif data == '0011001':
         return 1
-    if data == '0010011':
+    elif data == '0010011':
         return 2
-    if data == '0111101':
+    elif data == '0111101':
         return 3
-    if data == '0100011':
+    elif data == '0100011':
         return 4
-    if data == '0110001':
+    elif data == '0110001':
         return 5
-    if data == '0101111':
+    elif data == '0101111':
         return 6
-    if data == '0111011':
+    elif data == '0111011':
         return 7
-    if data == '0110111':
+    elif data == '0110111':
         return 8
-    if data == '0001011':
+    elif data == '0001011':
         return 9
+    else:
+        return -1
 
 
 # 2. Survey from the back and returns the index of the first 1 that appears.
